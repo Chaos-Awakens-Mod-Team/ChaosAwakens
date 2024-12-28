1a08016ee6ad27c36043dd2d879a81a04aa414c3
Alexandra-Myers <bia416cat@gmail.com>
2024-12-27 20:23:54 -0500

    Critter Caged

--- common/src/main/java/io/github/chaosawakens/common/item/misc/CritterCageItem.java
+++ common/src/main/java/io/github/chaosawakens/common/item/misc/CritterCageItem.java
@@ -1,7 +1,7 @@
 package io.github.chaosawakens.common.item.misc;
 
-import io.github.chaosawakens.util.RegistryUtil;
 import io.github.chaosawakens.util.MiscUtil;
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
@@ -12,7 +12,6 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.TamableAnimal;
-import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.animal.Wolf;
 import net.minecraft.world.entity.npc.Villager;
 import net.minecraft.world.entity.npc.VillagerProfession;
@@ -23,7 +22,6 @@ import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 
-import javax.annotation.Nonnull;
 import java.util.List;
 
 import static net.minecraft.world.entity.Entity.RemovalReason.DISCARDED;
@@ -49,60 +47,41 @@ public class CritterCageItem extends Item {
     public InteractionResult interactLivingEntity(ItemStack targetEmptyStack, Player playerOwner, LivingEntity target, InteractionHand hand) {
         ItemStack filledStack = getDefaultInstance();
 
-        if (shouldCaptureEntity(filledStack, playerOwner, target)) {
+        if (shouldCaptureEntity(filledStack, target)) {
             if (targetEmptyStack.getCount() == 1) {
-                if (!containsEntity(targetEmptyStack)) {
-                    playerOwner.setItemInHand(hand, filledStack);
-                    targetEmptyStack.shrink(1);
-                    target.remove(DISCARDED);
-                    return InteractionResult.SUCCESS;
-                } else return InteractionResult.FAIL;
-            } else {
-                if (!playerOwner.addItem(filledStack)) playerOwner.drop(filledStack, false, true);
-                targetEmptyStack.shrink(1);
-                target.remove(DISCARDED);
-                return InteractionResult.SUCCESS;
-            }
+                if (!containsEntity(targetEmptyStack)) playerOwner.setItemInHand(hand, filledStack);
+                else return InteractionResult.FAIL;
+            } else if (!playerOwner.addItem(filledStack)) playerOwner.drop(filledStack, false, true);
+            targetEmptyStack.shrink(1);
+            target.remove(DISCARDED);
+            return InteractionResult.SUCCESS;
         } else return InteractionResult.FAIL;
     }
-    private boolean shouldCaptureEntity(ItemStack targetStack, Player playerOwner, LivingEntity target) {
-        //if (target.getCommandSenderWorld().isClientSide) return false;
+    private boolean shouldCaptureEntity(ItemStack targetStack, LivingEntity target) {
+        if (target.level().isClientSide) return false;
         if (target instanceof Player || !target.isAlive()) return false;
-        if (containsEntity(targetStack)) return false;
         if (isBlacklisted(target.getType())) return false;
         if (target.getBbWidth() >= 3.35F || target.getBbHeight() >= 4.225F) return false; // Ent size or larger
 
         CompoundTag critterCageData = new CompoundTag();
 
-        critterCageData.putString("entity", EntityType.getKey(target.getType()).toString());
-        critterCageData.putString("entityName", target.getName().getString());
-        critterCageData.putDouble("entityMaxHealth", target.getAttribute(Attributes.MAX_HEALTH).getValue());
+        CompoundTag entityData = new CompoundTag();
+
+        target.stopRiding();
+        target.ejectPassengers();
+
+        target.save(entityData);
+        critterCageData.put("storedEntity", entityData);
         critterCageData.putBoolean("isBaby", target.isBaby());
-        critterCageData.putBoolean("isVillager", target instanceof Villager);
-        critterCageData.putBoolean("isTamable", target instanceof TamableAnimal);
+        critterCageData.putString("entityName", target.getName().getString());
 
         assert targetStack.getTag() != null;
-        if (critterCageData.getBoolean("isVillager")) {
-            Villager targetVillager = (Villager) target;
-
+        if (target instanceof Villager targetVillager) {
             critterCageData.putString("entityName", targetVillager.getName().getString() + " Villager");
-            critterCageData.putString("villagerProfession", targetVillager.getVillagerData().getProfession() != VillagerProfession.NONE ? MiscUtil.capitalizeFirstLetter(targetVillager.getVillagerData().getProfession().toString()) : "None");
-            critterCageData.putInt("villagerTradingLevel", targetVillager.getVillagerData().getLevel());
-        }
-        if (critterCageData.getBoolean("isTameable")) {
-            TamableAnimal targetTamable = (TamableAnimal) target;
-
-            critterCageData.putString("owner", targetTamable.getOwner() instanceof Player ? playerOwner.getScoreboardName() : "None");
-
-            if (targetTamable instanceof Wolf targetWolf) {
-                critterCageData.putString("collarColor", targetWolf.isTame() ? MiscUtil.capitalizeFirstLetter(targetWolf.getCollarColor().toString()) : "None");
-            }
         }
 
         critterCageData.putBoolean("enchanted", target.getType().toString().contains("enchanted"));
-        critterCageData.putString("entityRegName", RegistryUtil.getEntityKey(target.getType()).toString());
 
-        target.saveWithoutId(critterCageData);
         targetStack.setTag(critterCageData);
 
         targetStack.setCount(1);
@@ -128,36 +107,26 @@ public class CritterCageItem extends Item {
 
     private Entity getEntityFromStack(ItemStack targetStack, Level world, boolean withInfo) {
         if (targetStack.hasTag()) {
-            EntityType<?> type = EntityType.byString(targetStack.getTag().getString("entity")).orElse(null);
+            Entity targetEntity = EntityType.loadEntityRecursive(targetStack.getTag().getCompound("storedEntity"), world, entity -> entity);
+            if (!withInfo && !targetEntity.getType().canSummon()) return null;
 
-            if (type != null) {
-                LivingEntity targetEntity = (LivingEntity) type.create(world);
-
-                if (withInfo) targetEntity.load(targetStack.getTag());
-                else if (!type.canSummon()) return null;
-                return targetEntity;
-            }
+            return targetEntity;
         }
         return null;
     }
 
     private boolean containsEntity(ItemStack curStack) {
-        return curStack != null && !curStack.isEmpty() && curStack.hasTag() && curStack.getTag().contains("entity");
+        return curStack != null && !curStack.isEmpty() && curStack.hasTag() && curStack.getTag().contains("storedEntity");
     }
     @Override
     public boolean isFoil(ItemStack stack) {
         return stack.hasTag() && stack.getTag().contains("enchanted") && stack.getTag().getBoolean("enchanted");
     }
 
-    public String getMobID(ItemStack stack) {
-        return stack.getTag().getString("entity");
-    }
-
     public String getMobName(ItemStack stack) {
         return stack.getTag().getBoolean("isBaby") ? "Baby " +  stack.getTag().getString("entityName") : stack.getTag().getString("entityName");
     }
 
-    @Nonnull
     @Override
     public Component getName(ItemStack stack) {
         if (!containsEntity(stack)) return Component.translatable("item.chaosawakens.critter_cage");
@@ -167,37 +136,24 @@ public class CritterCageItem extends Item {
         if (containsEntity(targetStack)) {
             LivingEntity target = (LivingEntity) getEntityFromStack(targetStack, curWorld, true);
 
-            //	tooltip.add(Component.literal("Is Baby: " + stack.getTag().getBoolean("isBaby"))); -Unneeded for now --Meme Man
-            if (targetStack.getTag().getBoolean("isVillager")) {
-                if (target instanceof Villager) { //TODO What is this abomination of a switch statement :skullcry:
-                    switch (targetStack.getTag().getInt("villagerTradingLevel")) {
-                        case 1:
-                            if (targetStack.getTag().getInt("villagerTradingLevel") == 1 && !targetStack.getTag().getString("villagerProfession").equalsIgnoreCase("none"))
-                                tooltip.add(Component.literal("Trading Level: Novice"));
-                        case 2:
-                            if (targetStack.getTag().getInt("villagerTradingLevel") == 2)
-                                tooltip.add(Component.literal("Trading Level: Apprentice"));
-                        case 3:
-                            if (targetStack.getTag().getInt("villagerTradingLevel") == 3)
-                                tooltip.add(Component.literal("Trading Level: Journeyman"));
-                        case 4:
-                            if (targetStack.getTag().getInt("villagerTradingLevel") == 4)
-                                tooltip.add(Component.literal("Trading Level: Expert"));
-                        case 5:
-                            if (targetStack.getTag().getInt("villagerTradingLevel") == 5)
-                                tooltip.add(Component.literal("Trading Level: Master"));
-                        default:
-                            if (targetStack.getTag().getString("villagerProfession").equalsIgnoreCase("none"))
-                                tooltip.add(Component.literal("Trading Level: None"));
-                    }
-                }
+            if (target == null) return;
+            if (target.isBaby()) tooltip.add(Component.literal("Baby").withStyle(ChatFormatting.BLUE));
+            if (target instanceof Villager villager) {
+                String toAppend = switch (villager.getVillagerData().getLevel()) {
+                    case 1 -> "Novice";
+                    case 2 -> "Apprentice";
+                    case 3 -> "Journeyman";
+                    case 4 -> "Expert";
+                    case 5 -> "Master";
+                    default -> "";
+                };
+                if (villager.getVillagerData().getProfession().equals(VillagerProfession.NONE)) toAppend = "None";
+                if (!toAppend.isEmpty()) tooltip.add(Component.literal("Trading Level: ").withStyle(ChatFormatting.BLUE).append(Component.literal(toAppend).withStyle(ChatFormatting.GRAY)));
             }
-            if (targetStack.getTag().getBoolean("isTameable")) {
-                if (target instanceof TamableAnimal) {
-                    tooltip.add(Component.literal("Owner: " + targetStack.getTag().getString("owner")));
-                    if (target instanceof Wolf) {
-                        tooltip.add(Component.literal("Collar Color: " + targetStack.getTag().getString("collarColor")));
-                    }
+            if (target instanceof TamableAnimal targetTamable) {
+                tooltip.add(Component.literal("Owner: ").withStyle(ChatFormatting.BLUE).append(Component.literal(targetTamable.getOwner() instanceof Player player ? player.getScoreboardName() : "None").withStyle(ChatFormatting.GRAY)));
+                if (target instanceof Wolf targetWolf) {
+                    tooltip.add(Component.literal("Collar Color: ").withStyle(ChatFormatting.BLUE).append(Component.literal((targetWolf.isTame() ? MiscUtil.capitalizeFirstLetter(targetWolf.getCollarColor().toString()) : "None")).withStyle(ChatFormatting.GRAY)));
                 }
             }
         }
