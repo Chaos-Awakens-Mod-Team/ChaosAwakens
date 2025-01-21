471d214baf109c220b64bcaf78d1c0670bf3c207
Meme Man <81907436+RaveTr@users.noreply.github.com>
2025-01-21 23:12:22 +0200

    Port progress commit, check extended message for more info (99)
    - Progress on MP worldgen
    - Datagen re-run

--- common/src/main/java/io/github/chaosawakens/util/WorldGenUtil.java
+++ common/src/main/java/io/github/chaosawakens/util/WorldGenUtil.java
@@ -17,37 +17,34 @@ public final class WorldGenUtil {
     public static <C, I extends ToFloatFunction<C>> CubicSpline<C, I> miningParadiseOffset(I continentSplineCoord, I erosionSplineCoord, I foldedRidgesSplineCoord) {
         ToFloatFunction<Float> noTransform = NO_TRANSFORM;
 
-        CubicSpline<C, I> erosionOffsetSplineToDeepWater = TerrainProvider.buildErosionOffsetSpline(erosionSplineCoord, foldedRidgesSplineCoord, -0.112F, 0.0F, 0.01F, 0.0F, 0.0F, -0.04763F, false, false, noTransform);
-        CubicSpline<C, I> erosionOffsetSplineToShallowWater = TerrainProvider.buildErosionOffsetSpline(erosionSplineCoord, foldedRidgesSplineCoord, -0.223F, 0.03F, 0.1F, 0.124F, 0.01F, -0.0334F, false, false, noTransform);
-        CubicSpline<C, I> erosionOffsetSplineToLand = TerrainProvider.buildErosionOffsetSpline(erosionSplineCoord, foldedRidgesSplineCoord, -0.532F, 0.03F, 0.1F, 0.7F, 0.019F, -0.0334F, true, true, noTransform);
-        CubicSpline<C, I> erosionOffsetSplineToExtremeLand = TerrainProvider.buildErosionOffsetSpline(erosionSplineCoord, foldedRidgesSplineCoord, -1.008F, 0.056F, 0.1F, 1.0F, 0.02F, 0.011F, true, true, noTransform);
+        CubicSpline<C, I> erosionOffsetSplineFromDeepWater = TerrainProvider.buildErosionOffsetSpline(erosionSplineCoord, foldedRidgesSplineCoord, -0.312F, 0.0F, 0.01F, 0.0F, 0.0F, -0.02763F, false, false, noTransform);
+        CubicSpline<C, I> erosionOffsetSplineFromShallowWater = TerrainProvider.buildErosionOffsetSpline(erosionSplineCoord, foldedRidgesSplineCoord, -0.223F, 0.03F, 0.1F, 0.124F, 0.01F, -0.0334F, false, false, noTransform);
+        CubicSpline<C, I> erosionOffsetSplineFromLand = TerrainProvider.buildErosionOffsetSpline(erosionSplineCoord, foldedRidgesSplineCoord, -0.632F, 0.03F, 0.1F, 0.7F, 0.019F, -0.0334F, true, true, noTransform);
+        CubicSpline<C, I> erosionOffsetSplineFromExtremeLand = TerrainProvider.buildErosionOffsetSpline(erosionSplineCoord, foldedRidgesSplineCoord, -0.18F, 0.016F, 0.04F, 1.0F, 0.02F, 0.011F, true, true, noTransform);
 
-        return CubicSpline.builder(continentSplineCoord, noTransform)
-                .addPoint(-1.1F, 0.0084F)
-                .addPoint(-1.02F, -0.04222F)
+        return CubicSpline.builder(continentSplineCoord, noTransform) // Depth
+                .addPoint(-1.02F, 0.4222F)
                 .addPoint(-0.51F, -0.4222F)
                 .addPoint(-0.44F, -0.24F)
                 .addPoint(-0.18F, -0.024F)
-                .addPoint(-0.16F, erosionOffsetSplineToDeepWater)
-                .addPoint(-0.15F, erosionOffsetSplineToDeepWater)
-                .addPoint(-0.1F, erosionOffsetSplineToShallowWater)
-                .addPoint(0.25F, erosionOffsetSplineToLand)
-                .addPoint(1.0F, erosionOffsetSplineToExtremeLand)
+                .addPoint(-0.16F, erosionOffsetSplineFromDeepWater)
+                .addPoint(-0.01F, erosionOffsetSplineFromShallowWater)
+                .addPoint(0.25F, erosionOffsetSplineFromLand)
+                .addPoint(1.0F, erosionOffsetSplineFromExtremeLand)
                 .build();
     }
 
     public static <C, I extends ToFloatFunction<C>> CubicSpline<C, I> miningParadiseFactor(I continentSplineCoord, I erosionSplineCoord, I ridgesSplineCoord, I foldedRidgesSplineCoord) {
         ToFloatFunction<Float> noTransform =  NO_TRANSFORM;
 
-        CubicSpline<C, I> erosionFactorSpline = TerrainProvider.getErosionFactor(erosionSplineCoord, ridgesSplineCoord, foldedRidgesSplineCoord, 3.65F, true, noTransform);
-        CubicSpline<C, I> amplifiedErosionFactorSpline = TerrainProvider.getErosionFactor(erosionSplineCoord, ridgesSplineCoord, foldedRidgesSplineCoord, 2.97F, true, noTransform);
-        CubicSpline<C, I> erosionFactorSplineContinued = TerrainProvider.getErosionFactor(erosionSplineCoord, ridgesSplineCoord, foldedRidgesSplineCoord, 1.68F, true, noTransform);
-        CubicSpline<C, I> erosionFactorSplineFinal = TerrainProvider.getErosionFactor(erosionSplineCoord, ridgesSplineCoord, foldedRidgesSplineCoord, 1.19F, false, noTransform);
+        CubicSpline<C, I> erosionFactorSpline = TerrainProvider.getErosionFactor(continentSplineCoord, ridgesSplineCoord, foldedRidgesSplineCoord, 8.65F, true, noTransform);
+        CubicSpline<C, I> amplifiedErosionFactorSpline = TerrainProvider.getErosionFactor(continentSplineCoord, ridgesSplineCoord, foldedRidgesSplineCoord, 5.97F, true, noTransform);
+        CubicSpline<C, I> erosionFactorSplineContinued = TerrainProvider.getErosionFactor(continentSplineCoord, ridgesSplineCoord, foldedRidgesSplineCoord, 5.68F, true, noTransform);
+        CubicSpline<C, I> erosionFactorSplineFinal = TerrainProvider.getErosionFactor(continentSplineCoord, ridgesSplineCoord, foldedRidgesSplineCoord, 1.19F, false, noTransform);
 
-        return CubicSpline.builder(continentSplineCoord, NO_TRANSFORM)
-                .addPoint(-0.19F, 1.95F)
+        return CubicSpline.builder(erosionSplineCoord, NO_TRANSFORM)
+                .addPoint(-0.19F, 4.95F)
                 .addPoint(-0.15F, erosionFactorSpline)
-                .addPoint(-0.11F, 0.0F)
                 .addPoint(-0.1F, amplifiedErosionFactorSpline)
                 .addPoint(0.03F, erosionFactorSplineContinued)
                 .addPoint(0.06F, erosionFactorSplineFinal)
