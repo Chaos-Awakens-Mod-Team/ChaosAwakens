bc194fe31fdc67f6d069ec4558df045ae0d511d7
Meme Man <81907436+RaveTr@users.noreply.github.com>
2025-02-13 17:15:56 +0200

    Port progress commit, check extended message for more info (107)
    - Generic type patch for CAEntityTypes.java

--- common/src/main/java/io/github/chaosawakens/common/registry/CAEntityTypes.java
+++ common/src/main/java/io/github/chaosawakens/common/registry/CAEntityTypes.java
@@ -9,6 +9,7 @@ import io.github.chaosawakens.common.entity.hostile.robo.RoboPounder;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 
@@ -19,7 +20,7 @@ public class CAEntityTypes {
     private static final ObjectArrayList<Supplier<? extends EntityType<?>>> ENTITY_TYPES = new ObjectArrayList<>();
 
     // Robo
-    public static final Supplier<? extends EntityType<?>> ROBO_POUNDER = EntityTypePropertyWrapper.createGeneric(
+    public static final Supplier<EntityType<RoboPounder>> ROBO_POUNDER = EntityTypePropertyWrapper.create(
             registerEntityType("robo_pounder", () -> EntityType.Builder.of(RoboPounder::new, MobCategory.MONSTER)
                     .sized(4.1F, 6.0F)
                     .build(CAConstants.prefix("robo_pounder").toString())))
@@ -29,13 +30,13 @@ public class CAEntityTypes {
             .build()
             .getParentEntityType();
 
-    private static Supplier<? extends EntityType<?>> registerEntityType(ResourceLocation id, Supplier<? extends EntityType<?>> entityTypeSup) {
-        Supplier<? extends EntityType<?>> registeredEntityTypeSup = CAServices.REGISTRAR.registerObject(id, entityTypeSup, BuiltInRegistries.ENTITY_TYPE); // Otherwise reference to the entity type sup is null cuz it needs to be registered b4hand
+    private static <E extends Entity> Supplier<EntityType<E>> registerEntityType(ResourceLocation id, Supplier<EntityType<E>> entityTypeSup) {
+        Supplier<EntityType<E>> registeredEntityTypeSup = CAServices.REGISTRAR.registerObject(id, entityTypeSup, BuiltInRegistries.ENTITY_TYPE); // Otherwise reference to the entity type sup is null cuz it needs to be registered b4hand
         ENTITY_TYPES.add(registeredEntityTypeSup);
         return registeredEntityTypeSup;
     }
 
-    private static Supplier<? extends EntityType<?>> registerEntityType(String id, Supplier<? extends EntityType<?>> entityTypeSup) {
+    private static <E extends Entity> Supplier<EntityType<E>> registerEntityType(String id, Supplier<EntityType<E>> entityTypeSup) {
         return registerEntityType(CAConstants.prefix(id), entityTypeSup);
     }
 
